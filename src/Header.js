272986a8f67e1e196bbc8ef7782b0c41b88f04e4
Eric Ho <endonoho@gmail.com>
2020-08-31 16:20:57 -0700

    Add header skeleton layout

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,17 @@ import "./Header.scss";
 function Header() {
   return (
     <div className="header">
-      <div className='header__left'>
-      Avatars for logged in user
+      <div className="header__left">
+      {/* Avatars for logged in user */}
+      {/* History icon */}
+      </div>
+      <div className="header__search">
+        {/* Search iconx */}
+        {/* input */}
+      </div>
+      <div className="header__right">
+        {/* night-mode icon */}
+        {/* log out / log in / sign out */}
       </div>
     </div>
   )
