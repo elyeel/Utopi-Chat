866cb1f8c234a2c99ead55c8ba038f897753cf84
Eric Ho <endonoho@gmail.com>
2020-08-31 16:13:29 -0700

    initial commit so far

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className='header__left'>
+      Avatars for logged in user
+      </div>
+    </div>
+  )
+}
+
+export default Header;
