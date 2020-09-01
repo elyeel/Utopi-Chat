6c21667d95db2702b35dc010f1b4456ddb384411
Eric Ho <endonoho@gmail.com>
2020-08-31 17:20:15 -0700

    Implement Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
       <h1>UtopiChat</h1>
 
-      {/* Header */}
+      <Header />
       {/* Sidebar */}
       {/* React-Router -> Chat screen */}
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,40 @@
 import React from "react";
 import "./Header.scss";
 
+import { Avatar } from "@material-ui/core";
+import CommentIcon from '@material-ui/icons/Comment';
+import SearchIcon from '@material-ui/icons/Search';
+
+import ToggleOffIcon from '@material-ui/icons/ToggleOff';
+// import ToggleOnIcon from '@material-ui/icons/ToggleOn';
+
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
 function Header() {
   return (
     <div className="header">
       <div className="header__left">
-      {/* Avatars for logged in user */}
-      {/* History icon */}
+        <Avatar
+          className="header__avatar"
+          alt="Eric Ho"
+          src=""
+        />
+
+        <CommentIcon />
       </div>
+
       <div className="header__search">
-        {/* Search iconx */}
-        {/* input */}
+        <SearchIcon />
+
+        <input placeholder="Search for a channel" />
       </div>
+
       <div className="header__right">
         {/* night-mode icon */}
-        {/* log out / log in / sign out */}
+        <ToggleOffIcon className="header__right--toggle"/>
+
+        {/* log out */}
+        <ExitToAppIcon />
       </div>
     </div>
   )
