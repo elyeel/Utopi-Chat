e56f19374cc61b284ff5b19ff38c1e6c241f1747
Eric Ho <endonoho@gmail.com>
2020-09-01 20:20:17 -0700

    Add dotenv dependency and replace keys with variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,5 @@
 import firebase from "firebase";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyArPW2xAVLJli1c2pdqsDxB0OULeomXn5Q",
-//   authDomain: "utopichat.firebaseapp.com",
-//   databaseURL: "https://utopichat.firebaseio.com",
-//   projectId: "utopichat",
-//   storageBucket: "utopichat.appspot.com",
-//   messagingSenderId: "901854987491",
-//   appId: "1:901854987491:web:605b1be2951a464d0cf1d6",
-//   measurementId: "G-YCSHY1PEB6"
-// };
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
