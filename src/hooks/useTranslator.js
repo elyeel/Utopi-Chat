8ff847fd6044fba64fa0993e6666fbaef665f026
Eileen Li <eileenlimur@gmail.com>
2020-09-05 17:55:42 -0400

    create translate function that accepts message and language params and console.logs api response

--- src/hooks/useTranslator.js
+++ src/hooks/useTranslator.js
@@ -2,32 +2,30 @@ import uuidv4 from 'uuid/v4';
 import request from 'request';
 
 export default function useTranslator() {
-  let options = {
-    method: 'POST',
-    baseUrl: process.env.REACT_APP_TRANSLATOR_ENDPOINT,
-    url: 'translate',
-    qs: {
-      'api-version': '3.0',
-      'to': ['de', 'it']
-    },
-    headers: {
-      'Ocp-Apim-Subscription-Key': process.env.REACT_APP_TRANSLATOR_API_KEY,
-      'Content-type': 'application/json',
-      'X-ClientTraceId': uuidv4().toString()
-    },
-    body: [{
-      'text': 'Hello World!'
-    }],
-    json: true,
-  }
 
-  request(options, function(err, res, body) {
-    console.log(JSON.stringify(body, null, 4));
-  })
-
-  const translate = (message) => {
-    console.log(message);
-
+  const translate = (message, language) => {
+    let options = {
+      method: 'POST',
+      baseUrl: process.env.REACT_APP_TRANSLATOR_ENDPOINT,
+      url: 'translate',
+      qs: {
+        'api-version': '3.0',
+        'to': language
+      },
+      headers: {
+        'Ocp-Apim-Subscription-Key': process.env.REACT_APP_TRANSLATOR_API_KEY,
+        'Ocp-Apim-Subscription-Region': 'eastus',
+        'Content-type': 'application/json',
+        'X-ClientTraceId': uuidv4().toString()
+      },
+      body: [{
+        'text': message
+      }],
+      json: true,
+    }
+    request(options, function(err, res, body) {
+      console.log(JSON.stringify(body, null, 4));
+    })
   }
 
   return ({
