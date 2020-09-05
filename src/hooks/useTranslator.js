67f0d632cd50174f543cab71417356876e84a5dd
Eileen Li <eileenlimur@gmail.com>
2020-09-05 16:26:54 -0400

    add options object and request function

--- src/hooks/useTranslator.js
+++ src/hooks/useTranslator.js
@@ -1,9 +1,33 @@
-import axios from 'axios';
+import uuidv4 from 'uuid/v4';
+import request from 'request';
 
 export default function useTranslator() {
+  let options = {
+    method: 'POST',
+    baseUrl: process.env.REACT_APP_TRANSLATOR_ENDPOINT,
+    url: 'translate',
+    qs: {
+      'api-version': '3.0',
+      'to': ['de', 'it']
+    },
+    headers: {
+      'Ocp-Apim-Subscription-Key': process.env.REACT_APP_TRANSLATOR_API_KEY,
+      'Content-type': 'application/json',
+      'X-ClientTraceId': uuidv4().toString()
+    },
+    body: [{
+      'text': 'Hello World!'
+    }],
+    json: true,
+  }
+
+  request(options, function(err, res, body) {
+    console.log(JSON.stringify(body, null, 4));
+  })
 
   const translate = (message) => {
     console.log(message);
+
   }
 
   return ({
