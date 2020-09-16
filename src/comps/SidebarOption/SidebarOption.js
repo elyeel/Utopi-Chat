345c86b42df2b24ccbb265e1fb96c9866267d284
Risyardi Iswanto <elyeel@yahoo.com>
2020-09-15 18:50:30 -0700

    modifying # of online users feature by adding users to channelUsers db

--- src/comps/SidebarOption/SidebarOption.js
+++ src/comps/SidebarOption/SidebarOption.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SidebarOption.scss";
 import { useHistory } from "react-router-dom";
 // import db from "../../firebase";
@@ -14,42 +14,60 @@ function SidebarOption({
   db,
 }) {
   const history = useHistory();
-
-  // const checkUser = (name) => {
-  //   if (onlineUsers.findIndex(elem => elem.name === name) < 0) {
-  //     setOnlineUsers([
-  //       ...onlineUsers,
-  //       {
-  //         name: cookies.user.name,
-  //         picture: cookies.user.picture,
-  //         locale: cookies.user.locale,
-  //       },
-  //     ]);
-  //   }
-  // }
+  const [numUsers, setNumUsers] = useState(0);
 
   const selectChannel = () => {
     if (id) {
       history.push(`/channel/${id}`);
+      // check # of users online on this channel
+      db.collection("channelUsers")
+        .doc(id)
+        .get()
+        .then((doc) => {
+          const arrUsers = doc.data().users;
+          if (!arrUsers.some((e) => e === cookies.user.id)) {
+            arrUsers.push(cookies.user.id);
+            db.collection("channelUsers")
+              .doc(id)
+              .update({ users: arrUsers })
+              .then(() => setNumUsers(arrUsers.length))
+              .catch((error) =>
+                console.error("Error in updating the array of users!")
+              );
+          } else {
+            db.collection("channelUsers")
+              .doc(id)
+              .onSnapshot((snaps) => setNumUsers(snaps.data().users.length));
+          }
+          // doc.data().users.push(cookies.user.id);
+          // console.log(doc.data());
+        })
+        .catch((error) =>
+          console.error("Error in pushing a users into array", error)
+        );
     } else {
       history.push("title");
     }
-    // if (onlineUsers.length < 1) {
-    //   setOnlineUsers([
-    //     {
-    //       name: cookies.user.name,
-    //       picture: cookies.user.picture,
-    //       locale: cookies.user.locale,
-    //     },
-    //   ]);
-    // } else {
-    //   checkUser(cookies.user.name);
-    // }
-    db.collection("users")
-      .doc(cookies.user.id)
-      .update({ channelId: id })
-      .then((doc) => console.log("User updated ", doc))
-      .catch((error) => console.error("Error updating User", error));
+
+    // usersArr.push(cookies.user.id);
+    // db.collection("channelUsers")
+    //   .doc(id)
+    //   .update({ users: usersArr })
+    //   .then(() => console.log("Users array updated"))
+    //   .catch((error) => console.error("Error in updating", error));
+    // });
+
+    // db.collection("users")
+    //   .doc(cookies.user.id)
+    //   .update({ channelId: id })
+    //   .then((doc) => console.log("User updated ", doc))
+    //   .catch((error) => console.error("Error updating User", error));
+
+    // .then((users) => {
+    //   // const test = users;
+    //   // console.log(users);
+    // })
+    // .catch((error) => console.error("Can't get channel users data", error));
   };
 
   const addChannel = () => {
@@ -59,24 +77,52 @@ function SidebarOption({
       db.collection("channels").add({
         name: channelName,
       });
+      db.collection("channelUsers")
+        .doc(id)
+        .set({
+          id,
+          users: [],
+        })
+        .then((doc) => console.log(doc))
+        .catch((error) =>
+          console.error("Error adding online users by channel", error)
+        );
     }
   };
 
+  // const usersOnline = () => {
+  //   let num = 0;
+  //   db.collection("channelUsers")
+  //     .doc(id)
+  //     .onSnapshot((snaps) => {
+  //       num = snaps.data().users.length;
+  //       setNumUsers(snaps.data().users.length);
+  //     });
+  //   return num;
+  // };
+
   return (
-    <div
-      className="sidebarOption"
-      onClick={addChannelOption ? addChannel : selectChannel}
-    >
-      {Icon && <Icon className="sidebarOption__icon" />}
-      {Icon ? (
-        <h3>{title}</h3>
-      ) : (
-        <h3 className="sidebarOption__channel">
-          <span className="sidebarOption__hash">#</span>
-          {title}
-        </h3>
-      )}
-    </div>
+    <>
+      <div
+        className="sidebarOption"
+        onClick={addChannelOption ? addChannel : selectChannel}
+      >
+        {Icon && <Icon className="sidebarOption__icon" />}
+        {Icon ? (
+          <h3>{title}</h3>
+        ) : (
+          <h3 className="sidebarOption__channel">
+            <span className="sidebarOption__hash">#</span>
+            {title}
+            {id && (
+              <span className="sidebarOption__numUsers">
+                {numUsers}
+              </span>
+            )}
+          </h3>
+        )}
+      </div>
+    </>
   );
 }
 
