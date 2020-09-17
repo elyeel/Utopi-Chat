fddd471f42e26e425cc060a39d0cdcc595511b80
Eileen Li <eileenlimur@gmail.com>
2020-09-16 23:58:05 -0400

    modify translate function to ping Utopi-Chat-Server

--- src/helpers/translate.js
+++ src/helpers/translate.js
@@ -1,29 +1,11 @@
-const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
-const { IamAuthenticator } = require('ibm-watson/auth');
+import axios from 'axios';
 
-export default function translate() {
+export default function translate(request) {
 
-  const languageTranslator = new LanguageTranslatorV3({
-    version: '2018-05-01',
-    authenticator: new IamAuthenticator({
-      apikey: `${process.env.REACT_APP_TRANSLATOR_API_KEY}`,
-    }),
-    serviceUrl: `${process.env.REACT_APP_TRANSLATOR_ENDPOINT}`,
-    headers: {
-      "Content-Type": "application/json"
-    },
-  });
-
-  const translateParams = {
-    text: 'Hello, how are you today?',
-    target: 'es'
-  }
-
-  return languageTranslator.translate(translateParams)
-    .then(translationResult => {
-      console.log(JSON.stringify(translationResult, null, 2));
-    })
-    .catch(err => {
-      console.log('error:', err);
-    });
+  return axios.post('/api/translate', {
+    text: request.text,
+    target_language: request.target_language
+  })
+  .then(response => console.log(response))
+  .catch(err => console.log(err));
 }
