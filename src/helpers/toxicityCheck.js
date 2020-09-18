628fdea3a7bcc4d2233e41ed8e88dc8d46e735a0
Eileen Li <eileenlimur@gmail.com>
2020-09-17 20:09:19 -0400

    return promise from toxicityCheck function to resolve previous undefined asyncronous return from nested promise

--- src/helpers/toxicityCheck.js
+++ src/helpers/toxicityCheck.js
@@ -1,13 +1,13 @@
 import * as tf from '@tensorflow/tfjs';
 import * as toxicity from '@tensorflow-models/toxicity';
 
-export default function toxicityCheck(text, callback) {
+export default function toxicityCheck(text) {
   const threshold = 0.9;
-  
-  toxicity.load(threshold).then(model => {
+  return toxicity.load(threshold)
+  .then(model => {
     const sentences = [text];
-    model.classify(sentences).then(predictions => {    
-      callback(predictions);
+    return model.classify(sentences)
+    .then(predictions => {    
       return predictions[6].results[0].match === true;
     })
   })
