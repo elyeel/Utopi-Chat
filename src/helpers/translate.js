7e0d1e7439cd774d1c54670df714af0baa1cd8bf
Eileen Li <eileenlimur@gmail.com>
2020-09-20 15:58:02 -0400

    add error handling

--- src/helpers/translate.js
+++ src/helpers/translate.js
@@ -5,7 +5,18 @@ export default function translate(request) {
     text: request.text,
     target_language: request.target_language
   })
-  // .then(response => response.data.translations[0])
-  .then(response => response.data.result.translations[0].translation)
+  .then(response => {
+    if (response.data.result) {
+      return response.data.result.translations[0].translation
+    } else {
+      if (response.data === 'Automatically detected source language is the same as target, cannot translate. Try to set the source language explicitly if you think the source language was not correctly detected') {
+        return 'N/A';
+      } else if (response.data === 'Unable to automatically detect the source language, confidence too low') {
+        return 'No Result';
+      } else {
+        return response.data;
+      }
+    }
+  })
   .catch(err => console.log(err));
 }
