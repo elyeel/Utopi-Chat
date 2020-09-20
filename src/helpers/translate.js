81cdc6f012a270e1b7d79635367946d23390ef07
Eileen Li <eileenlimur@gmail.com>
2020-09-20 15:18:02 -0400

    return proper response from translate function

--- src/helpers/translate.js
+++ src/helpers/translate.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export default function translate(request) {
-
   return axios.post('/api/translate', {
     text: request.text,
     target_language: request.target_language
   })
-  .then(response => console.log(response))
+  // .then(response => response.data.translations[0])
+  .then(response => response.data.result.translations[0].translation)
   .catch(err => console.log(err));
 }
