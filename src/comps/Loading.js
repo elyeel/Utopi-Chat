f4ea90a49e0ea7f588c4c5cadac738853659e810
Eileen Li <eileenlimur@gmail.com>
2020-09-20 15:57:31 -0400

    create loading component

---
+++ src/comps/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+
+export default function Loading ({ type, color }) {
+  return (
+    <ReactLoading type={type} color={color} height={50} width={20} />
+  )
+}
+
