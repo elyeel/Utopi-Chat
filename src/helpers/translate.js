9f86f30fb1e8b6b0009c9dc69c8c058497dd9015
Eileen Li <eileenlimur@gmail.com>
2020-09-23 19:14:25 -0400

    replace error message with red question mark

--- src/helpers/translate.js
+++ src/helpers/translate.js
@@ -7,15 +7,15 @@ export default function translate(request) {
   })
   .then(response => {
     if (response.data.result) {
-      return response.data.result.translations[0].translation
+      return response.data.result.translations[0].translation;
     } else {
-      if (response.data === 'Automatically detected source language is the same as target, cannot translate. Try to set the source language explicitly if you think the source language was not correctly detected') {
-        return 'N/A';
-      } else if (response.data === 'Unable to automatically detect the source language, confidence too low') {
-        return 'No Result';
-      } else {
-        return response.data;
-      }
+      // if (response.data.message === 'Automatically detected source language is the same as target, cannot translate. Try to set the source language explicitly if you think the source language was not correctly detected') {
+        // return 'N/A';
+      // } else if (response.data.message === 'Unable to automatically detect the source language, confidence too low') {
+        // return 'No Result';
+      // } else {
+        return '❓';
+      // }
     }
   })
   .catch(err => console.log(err));
