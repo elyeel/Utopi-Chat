0d2d4a5b1cde42fbe6a634eb9dcbdb4ab0823bf7
Eileen Li <eileenlimur@gmail.com>
2020-10-02 00:50:54 -0400

    Add if/else statement outside existing message-send logic and put showAlert() (prop) inside if statement

--- src/comps/ChatForm/ChatForm.js
+++ src/comps/ChatForm/ChatForm.js
@@ -7,56 +7,60 @@ import Button from "@material-ui/core/Button";
 import toxicityCheck from "../../helpers/toxicityCheck";
 import ToxicityWarningModal from "../ToxicityWarningModal/ToxicityWarningModal";
 
-export default function ChatForm({ channelId, channelName }) {
+export default function ChatForm({ channelId, channelName, showAlert }) {
   const [cookies] = useCookies(["user"]);
   const [msg, setMsg] = useState("");
   const [modal, setModal] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const submitMsg = (event) => {
+const submitMsg = (event) => {
     event.preventDefault();
-    setLoading(true);
-    toxicityCheck(msg).then((res) => {
-      setLoading(false);
-      if (res) {
-        setModal(true);
-        setMsg("");
-      } else {
-        db.collection("channels")
-          .doc(channelId)
-          .collection("messages")
-          .add({
-            message: msg,
-            timestamp: new Date(Date.now()),
-            user: cookies.user.name,
-            userimage: cookies.user.picture,
-          })
-          .then((docRef) => {
-            const tempArr = [];
-            console.log("Written with id :", docRef.id);
-            setMsg("");
-            db.collection("channels")
-              .doc(channelId)
-              .collection("messages")
-              .get()
-              .then((msgs) => {
-                for (let msg of msgs.docs) {
-                  tempArr.push({
-                    messageId: msg.id,
-                    message: msg.data().message,
-                    timestamp: msg.data().timestamp.toDate(),
-                    user: msg.data().user,
-                    userimage: msg.data().userimage,
-                  });
-                }
-              })
-              .then(() =>
-                localStorage.setItem(channelId, JSON.stringify(tempArr))
-              );
-          })
-          .catch((error) => console.error("Error adding message: ", error));
-      }
-    });
+    if (msg.trim() === "") {
+      showAlert();
+    } else {
+      setLoading(true);
+      toxicityCheck(msg).then((res) => {
+        if (res) {
+          setModal(true);
+          setMsg("");
+        } else {
+          db.collection("channels")
+            .doc(channelId)
+            .collection("messages")
+            .add({
+              message: msg,
+              timestamp: new Date(Date.now()),
+              user: cookies.user.name,
+              userimage: cookies.user.picture,
+            })
+            .then((docRef) => {
+              const tempArr = [];
+              console.log("Written with id :", docRef.id);
+              setMsg("");
+              db.collection("channels")
+                .doc(channelId)
+                .collection("messages")
+                .get()
+                .then((msgs) => {
+                  for (let msg of msgs.docs) {
+                    tempArr.push({
+                      messageId: msg.id,
+                      message: msg.data().message,
+                      timestamp: msg.data().timestamp.toDate(),
+                      user: msg.data().user,
+                      userimage: msg.data().userimage,
+                    });
+                  }
+                })
+                .then(() =>
+                  localStorage.setItem(channelId, JSON.stringify(tempArr))
+                );
+            })
+            .catch((error) => console.error("Error adding message: ", error));
+        setLoading(false);
+        }
+      });
+    }
   };
 
   return (
